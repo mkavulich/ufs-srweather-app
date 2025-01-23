5dced13eafaea9c6a195f19f9e5e8cfb16e309bb
Michael Kavulich, Jr. <kavulich@ucar.edu>
2025-01-23 21:12:13 +0000

    Local changes for AMS work, including pygraf in workflow

--- scripts/exregional_get_verif_obs.sh
+++ scripts/exregional_get_verif_obs.sh
@@ -51,6 +51,7 @@ done
 #
 #-----------------------------------------------------------------------
 #
+set -x
 valid_obtypes=("CCPA" "MRMS" "NDAS" "NOHRSC" "AERONET" "AIRNOW")
 if [[ ! ${valid_obtypes[@]} =~ ${OBTYPE} ]]; then
   print_err_msg_exit "\
@@ -65,6 +66,7 @@ cmd="\
 python3 -u ${USHdir}/get_obs.py \
 --var_defns_path "${GLOBAL_VAR_DEFNS_FP}" \
 --obtype ${OBTYPE} \
+--log_level DEBUG \
 --obs_day ${PDY}"
 print_info_msg "
 CALLING: ${cmd}"
